01ec4876127f14ea5701a0a298810f8b5c686799
karianjahi <kitgaedesigaidi8@gmail.com>
2025-11-18 19:14:36 +0100

    Create some comments inside the source file

---
+++ source.js
@@ -0,0 +1,11 @@
+/*
+Implementing a Value Remover Function
+In this lab, I create a function that takes an initial array as its first argument, followed by one or more additional arguments representing the values to remove.
+
+User Stories:
+
+1. I create a `destroyer` function that accepts an array and one or more additional arguments.
+2. The `destroyer` function returns a new array excluding all elements from the first argument that match any of the subsequent arguments.
+3. The function accepts an indeterminate number of arguments.
+
+*/
