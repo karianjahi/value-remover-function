a5bc3c39219a045d2873e7832f7f5bcb31628782
karianjahi <kitgaedesigaidi8@gmail.com>
2025-11-18 20:00:44 +0100

    Populate the function with comments, function details and tests which pass

--- source.js
+++ source.js
@@ -9,3 +9,17 @@ User Stories:
 3. The function accepts an indeterminate number of arguments.
 
 */
+
+const destroyer = (anArray, ...args) => {
+    return anArray.filter((num) => !args.includes(num))
+}
+
+
+// Tests
+console.log(destroyer([2, 3, 5, 7, 1], 2, 5, 1, 5)); // => [ 3, 7 ]
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3)); // => [ 1, 1 ]
+console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3)); // => [ 1, 5, 1 ]
+console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5)); // => [ 1 ]
+console.log(destroyer([2, 3, 2, 3], 2, 3)); // => []
+console.log(destroyer(["tree", "hamburger", 53], "tree", 53)); // => [ 'hamburger' ]
+console.log(destroyer( ["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan" )); // => [ 12, 92, 65 ]
